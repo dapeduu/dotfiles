d6850c096f647d905b96c16fa99a9a05e1cb9145
Pedro Henrique dos Santos <pedrosantosdevelop@gmail.com>
2022-08-08 00:43:49 -0300

    Update dot_local/share/chezmoi/literal_run_once_install-packages.sh Rename chezmoi.toml to dot_local/share/chezmoi/chezmoi.toml

--- dot_local/share/chezmoi/literal_run_once_install-packages.sh
+++ dot_local/share/chezmoi/literal_run_once_install-packages.sh
@@ -3,20 +3,31 @@
 main() {
   apt_update
   install_vscode
+  install_zsh
 }
 
 apt_update() {
  sudo apt-get update
 }
 
+# https://code.visualstudio.com/docs/setup/linux
 install_vscode() {
- sudo apt-get install wget gpg
+ sudo apt-get install wget gpg apt-transport-https
  wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
  sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
  sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
  rm -f packages.microsoft.gpg
- sudo apt-get install apt-transport-https
+ apt_update
  sudo apt-get install code
+}
+
+install_zsh() {
+  sudo apt-get install zsh
+}
+
+# https://github.com/ohmyzsh/ohmyzsh#unattended-install
+install_oh_my_zsh() { 
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 }
 
 main "$@"; exit
