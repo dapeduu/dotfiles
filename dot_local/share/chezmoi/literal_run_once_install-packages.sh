6cfa5f3bd8b9b0a5c8f9f7142fc683b7518cbd76
Pedro Henrique dos Santos <pedrosantosdevelop@gmail.com>
2022-08-08 21:39:12 -0300

    Add chezmoi.toml Update dot_local/share/chezmoi/literal_run_once_install-packages.sh Update run_once_install-packages.sh

--- dot_local/share/chezmoi/literal_run_once_install-packages.sh
+++ dot_local/share/chezmoi/literal_run_once_install-packages.sh
@@ -13,22 +13,26 @@ apt_update() {
 
 # https://code.visualstudio.com/docs/setup/linux
 install_vscode() {
- sudo apt-get install wget gpg apt-transport-https
+ sudo apt-get install wget gpg apt-transport-https -y
  wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
  sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
  sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
  rm -f packages.microsoft.gpg
  apt_update
- sudo apt-get install code
+ sudo apt-get install code -y
 }
 
 install_zsh() {
-  sudo apt-get install zsh
+  sudo apt-get install zsh -y
   chsh -s $(which zsh) # Make it the default shell
 }
 
 # https://github.com/ohmyzsh/ohmyzsh#unattended-install
 install_oh_my_zsh() { 
+  OH_MY_ZSH=~/.oh-my-zsh
+  if [-d "$OH_MY_ZSH" ]; then return; fi
+
+  sudo apt-get install git -y
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 }
 
--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -3,20 +3,33 @@
 main() {
   apt_update
   install_vscode
+  install_zsh
+  install_oh_my_zsh
 }
 
 apt_update() {
  sudo apt-get update
 }
 
+# https://code.visualstudio.com/docs/setup/linux
 install_vscode() {
- sudo apt-get install wget gpg
+ sudo apt-get install wget gpg apt-transport-https -y
  wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
  sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
  sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
  rm -f packages.microsoft.gpg
- sudo apt-get install apt-transport-https
- sudo apt-get install code
+ apt_update
+ sudo apt-get install code -y
+}
+
+install_zsh() {
+  sudo apt-get install zsh -y
+  chsh -s $(which zsh) # Make it the default shell
+}
+
+# https://github.com/ohmyzsh/ohmyzsh#unattended-install
+install_oh_my_zsh() { 
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 }
 
 main "$@"; exit
